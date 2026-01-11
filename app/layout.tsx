f5fce9bcc67901d50746c985779aa21dbcefab20
Specode <alin@topflightapps.com>
2026-01-11 15:42:09 +0000

    Add Footer component to root layout to display on all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { SpecodeIframeTracker } from "@core/integrations/specode";
 import { ClientNotificationProvider } from "@/features/notifications";
 import { PharmacyProvider } from "@/contexts/PharmacyContext";
+import { Footer } from "@/components/layout/Footer";
 import "./theme.css";
 
 const inter = Inter({
@@ -38,6 +39,7 @@ export default function RootLayout({
               <div className="flex-1 flex flex-col">
                 {children}
               </div>
+              <Footer />
               <Toaster />
             </ClientNotificationProvider>
           </PharmacyProvider>
