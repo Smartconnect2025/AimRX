62f6872ba8eeee577510b094d18e283c4442918c
Tomas Goldenberg <ext.tgoldenberg@riotgames.com>
2026-01-12 17:52:50 -0300

    add patient landing flow

---
+++ features/intake/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Patient Intake Feature
+ *
+ * Multi-step intake flow for new patients.
+ * Steps: Patient Info → Medical History → Insurance → Consent
+ */
+
+export { IntakeLayout } from "./components/IntakeLayout";
+export { PatientInfoForm } from "./components/PatientInfoForm";
+export { MedicalHistoryForm } from "./components/MedicalHistoryForm";
+export { InsuranceForm } from "./components/InsuranceForm";
+export { ConsentForm } from "./components/ConsentForm";
+
+export { useIntakeProgress } from "./hooks/useIntakeProgress";
+
+export * from "./types";
