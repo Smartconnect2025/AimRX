6c3fd6c63ed0024182f1666a743a1877e06e73c7
Tomas Goldenberg <ext.tgoldenberg@riotgames.com>
2026-01-26 12:43:39 -0300

    improved report

--- app/(features)/admin/pharmacy-reports/page.tsx
+++ app/(features)/admin/pharmacy-reports/page.tsx
@@ -333,7 +333,7 @@ export default function PharmacyReportsPage() {
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
                 <Input
                   id="search"
-                  placeholder="Patient, medication..."
+                  placeholder="Patient"
                   value={searchTerm}
                   onChange={(e) => setSearchTerm(e.target.value)}
                   className="pl-10"
