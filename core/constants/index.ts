b8be9cee4d144cec1aea0d954c4705c73dd873f2
Tomas Goldenberg <ext.tgoldenberg@riotgames.com>
2026-02-04 14:32:22 -0300

    fix default pharmacy

--- core/constants/index.ts
+++ core/constants/index.ts
@@ -1,5 +1,11 @@
 // Timezone constants and utilities
-export * from './timezones';
+export * from "./timezones";
 
 // US States constants and utilities
-export * from './us-states'; 
+export * from "./us-states";
+
+export const DEFAULT_PHARMACY_STORE_ID = "190670";
+
+export const DEFAULT_PHARMACY_SYSTEM_TYPE = "DigitalRx";
+
+export const DEFAULT_PHARMACY_SLUG = "ecp";
